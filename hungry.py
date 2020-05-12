e9de5c6a6971a8aa97c91da47f59972eaf1514cb
gmrokz <gpl31.msh@gmail.com>
2020-05-12 10:52:04 +0530

    WATER

--- hungry.py
+++ hungry.py
@@ -4,4 +4,6 @@ if hungry == "yes":
     print("eat pizza!")
 
 else:
-    print("do your homework!")
+    thirsty=input("are you thirsty!")
+    if thirsty=="yes":
+         print("drink water buddy!!")
