2d55f53ef6a6f4c177bc27bdd629b8b6f11d716d
gmrokz <gpl31.msh@gmail.com>
2020-06-13 14:18:46 +0530

    munchis changes

--- hungry.py
+++ hungry.py
@@ -2,7 +2,7 @@
 hungry=input("are you hungry?")
 if hungry == "yes" and hungry=="y":
     print("eat samosa")
-    print("eat munchis!")
+    print("eat pizza!")
     print("eat fries")
     print("eat burger")
 else:
