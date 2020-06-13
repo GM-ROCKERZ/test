ae9d868aeb33d9ade242efecc396a0fcd1acb8b1
gmrokz <gpl31.msh@gmail.com>
2020-06-13 13:37:21 +0530

    python code

--- hungry.py
+++ hungry.py
@@ -1,10 +1,10 @@
+# TODO make few changes on the code
 hungry=input("are you hungry?")
 if hungry == "yes" and hungry=="y":
     print("eat samosa")
     print("eat pizza!")
     print("eat fries")
     print("eat burger")
-
 else:
     thirsty=input("are you thirsty!")
     if thirsty=="yes":
